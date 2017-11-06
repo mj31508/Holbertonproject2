6a2197d7aa240015ce6fb5e078ee1b6eb5a79124
Max <mj31508@gmail.com>
2017-11-05 22:52:01 -0500

    first commit

---
+++ projectjsfiles.js
@@ -0,0 +1,7 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.2.0/imagesloaded.pkgd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/plugins/ScrollToPlugin.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+<script src="https://ihatetomatoes.net/demos/scrollmagic-templates/scrollmagic-template-04/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
